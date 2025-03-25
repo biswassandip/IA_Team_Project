d7e679c77a6589425eaafb7edbef61777cd5c9ae
Sandip Biswas <bizz.sand@gmail.com>
2025-03-25 09:54:20 +0000

    Delete msc_ia_bot/src/gui directory

--- msc_ia_bot/src/gui/trials/bard_ngl.py
+++
@@ -1,8 +0,0 @@
-import nglview
-
-# Initialize a Structure object with a PDB file path and a list of additional options using the `nglview.Structure.from_file()` method
-structure = nglview.Structure.from_file("rna_structure.pdb", {"colorScheme": "rainbow"})
-
-# Display the structure
-view = nglview.View(structure)
-view.show()
